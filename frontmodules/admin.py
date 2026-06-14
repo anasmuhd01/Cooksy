0037a6d246cd41cdf9570e57e22986e22afef472
anasmuhd01 <anasmuhd010@gmail.com>
2026-06-15 00:02:35 +0530

    model test

--- frontmodules/admin.py
+++ frontmodules/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from frontmodules.models import *
 
 # Register your models here.
+
+admin.site.register(Recipie)
+admin.site.register(Ingredient)
+admin.site.register(ReciepieItem)
--- frontmodules/models.py
+++ frontmodules/models.py
@@ -6,11 +6,20 @@ class Recipie(models.Model):
     recipie_name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.recipie_name
+
 class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.ingredient_name
+
 class ReciepieItem(models.Model):
-    recipie_name = models.ForeignKey(Recipie,on_delete=models.CASCADE)
-    ingredient_name = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
+    recipie = models.ForeignKey(Recipie,on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient,on_delete=models.CASCADE)
     quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.recipie.recipie_name}-{self.ingredient.ingredient_name}"
