43221808a9bb24098eafe2e591769f496a7b0054
anasmuhd01 <anasmuhd010@gmail.com>
2026-06-16 00:09:16 +0530

    ingredient list

--- cooksy/urls.py
+++ cooksy/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from frontmodules.views import IngredientListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ilist',IngredientListView.as_view()),
 ]
--- frontmodules/views.py
+++ frontmodules/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from frontmodules.models import Ingredient
+from django.views import View
+
 
 # Create your views here.
+
+class IngredientListView(View):
+    def get(self,req):
+        ingredients = Ingredient.objects.all()
+        return render(req,'ingredientlist.html',{'ingredients':ingredients})
